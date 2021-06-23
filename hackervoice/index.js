9ac52bc587116c2e5297d7a06de76c208c4a2667
danielkimtest <autumnleaf023@gmail.com>
2021-06-22 23:34:36 -0400

    updated hackervoice function
    tests for correct password = letmein

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,9 +2,12 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var password = req.query.password
+    var response = "Access denied."
+    if (password == "letmein")
+        response = "Access granted."
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: response
     };
 }
