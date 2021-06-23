f8478cf7e8b37fdbb47484f7ca48b9cf10688d87
danielkimtest <autumnleaf023@gmail.com>
2021-06-22 23:37:33 -0400

    fixing hackervoice

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,6 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var password = req.query.password
+    
     var response = "Access denied."
     if (password == "letmein")
         response = "Access granted."
