6264a378668ce72e56be78e397b2b826b2349c2c
danielkimtest <autumnleaf023@gmail.com>
2021-06-29 15:49:26 -0400

    updated emotional gifs to post image to face api and receive information about emotions

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -4,9 +4,36 @@ module.exports = async function (context, req) {
     var boundary = multipart.getBoundary(req.headers['content-type']);
     var body = req.body;
     var parts = multipart.Parse(body, boundary);
-    var convertedResult = Buffer.from(parts[0].data).toString('base64');
+
+    var result = await analyzeImage(parts[0].data);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
+	    body: {
+		    result
+	    }
     };
+    console.log(result)
+    context.done(); 
+}
+
+async function analyzeImage(img){
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'    
+    })
+
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',
+        body: img,  
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+
+    let data = await resp.json();
+    
+    return data; 
 }
