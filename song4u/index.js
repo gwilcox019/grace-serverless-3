6b11933eba537b1a0caa37c4434f35bb64dc865e
danielkimtest <autumnleaf023@gmail.com>
2021-06-29 19:28:26 -0400

    added song return to song4u function

--- song4u/index.js
+++ song4u/index.js
@@ -28,8 +28,17 @@ module.exports = async function (context, req) {
     else    
         id = "Unkown";
 
+    const songs = {"GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+        "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+        "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+        "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+        "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+
+    let song = song[id];
+    let message = `We guessed you're part of this generation: ${id}! Happy listening! ${song}`;
+    
     context.res = {
-        body: id
+        body: message
     };
 }
 
