555f69033c1992e5afdb3c2a6cfee850dd03f359
danielkimtest <autumnleaf023@gmail.com>
2021-07-06 21:01:22 -0400

    had to add specifications between jpg and jpEg

--- bunnimage-download/index.js
+++ bunnimage-download/index.js
@@ -5,6 +5,7 @@ module.exports = async function (context, req) {
     var download = ""
     var downloadpng = "https://wilcoxserverless1.blob.core.windows.net/wilcoxserverless/" + username + ".png";
     var downloadjpg = "https://wilcoxserverless1.blob.core.windows.net/wilcoxserverless/" + username + ".jpg";
+    var downloadjpeg = "https://wilcoxserverless1.blob.core.windows.net/wilcoxserverless/" + username + ".jpeg";
 
     let pngresp = await fetch(downloadpng, {
         method: 'GET',
@@ -15,11 +16,18 @@ module.exports = async function (context, req) {
         method: 'GET',
     })
     let jpgdata = await jpgresp;
+
+    let jpegresp = await fetch(downloadjpeg, {
+        method: 'GET',
+    })
+    let jpegdata = await jpegresp;
      
-    if (pngdata.statusText == "The specified blob does not exist." && jpgdata.statusText == "The specified blob does not exist." ) {
+    if (pngdata.statusText == "The specified blob does not exist." && 
+    jpgdata.statusText == "The specified blob does not exist." && jpegdata.statusText == "The specified blob does not exist.") {
         success = false;
         context.log("Does not exist: " + pngdata)
         context.log("Does not exist: " + jpgdata)
+        context.log("Does not exist: " + jpegdata)
     } else if (pngdata.statusText != "The specified blob does not exist.") {
         success = true;
         download = downloadpng
@@ -27,6 +35,10 @@ module.exports = async function (context, req) {
     } else if (jpgdata.statusText != "The specified blob does not exist.") {
         success = true;
         download = downloadjpg
+        context.log("Does exist: " + jpgdata)
+    } else if (jpegdata.statusText != "The specified blob does not exist.") {
+        success = true;
+        download = downloadjpeg
         context.log("Does exist: " + jpgdata)
     }
      
--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -21,8 +21,10 @@ module.exports = async function (context, req) {
         var filetype = parsedBody[0].type;
         if (filetype == "image/png") {
          ext = "png";
-        } else if (filetype == "image/jpeg") {
+        } else if (filetype == "image/jpg") {
             ext = "jpg";
+        } else if (filetype == "image/jpeg") {
+            ext = "jpeg";
         } else {
             username = "invalidimage"
             ext = "";
