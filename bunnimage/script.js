9206a076d7fb8d7c11d606aa57fbdb59e8e84fb8
danielkimtest <autumnleaf023@gmail.com>
2021-07-13 12:32:11 -0400

    uploading images to azure

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,7 +1,33 @@
+const { pathToFileURL } = require("node:url");
+const fetch = require('node-fetch');
+
 function getImage(event) {
     event.preventDefault();
-    if(document.getElementById('name').value != '')
+    if(document.getElementById('name').value != ''){
         $('#output').text("Thanks!");
-    else
+    }
+    else {
         alert("No name error.");
+    }
+    
+    var bunniForm = document.getElementById("myform");
+    var payload = new FormData(bunniForm);
+    const file = fileInput.files[0]; // fileInput is the file upload input element
+    payload.append(document.getElementById('name').value, file);
+    
+    var myHeaders = new Headers();
+    myHeaders.appends('content-type', 'file')
+    myHeaders.append('codename', document.getElementById('name').value)
+    try {
+        const response = await fetch(process.env.bunnimage_upload_url, {
+            method: 'POST',
+            body: payload, 
+            headers: myHeaders
+         });         
+    } catch (e) {
+        alert("an error occurred");
+    }
+
+    $('#output').text("Your image has been stored successfully!");
+    
 }
