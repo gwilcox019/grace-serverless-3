befd230578b1fbbb8b4bf6c7deaa2a517d8aa7f1
danielkimtest <autumnleaf023@gmail.com>
2021-07-14 22:53:00 -0400

    four cats

--- twocatz/index.js
+++ twocatz/index.js
@@ -1,36 +1,40 @@
 const fetch = require('node-fetch')
 
 module.exports = async function (context, req) {
-    let resp1 = await fetch("https://251a9d93-8c22-472d-b5c5-c0b013b15125.mock.pstmn.io/bitproject", {
+
+    let resp1 = await fetch("https://cataas.com/cat/says/" + req.query.name1, {
         method: 'GET'
     });
 
-    let resp2 = await fetch("https://251a9d93-8c22-472d-b5c5-c0b013b15125.mock.pstmn.io/bitproject", {
+    let resp2 = await fetch("https://cataas.com/cat/says/" + req.query.name2, {
+        method: 'GET'
+    });
+
+    let resp3 = await fetch("https://cataas.com/cat/says/" + req.query.name3, {
+        method: 'GET'
+    });
+
+    let resp4 = await fetch("https://cataas.com/cat/says/" + req.query.name4, {
         method: 'GET'
     });
 
     let data1 = await resp1.arrayBuffer();
     let data2 = await resp2.arrayBuffer();
+    let data3 = await resp3.arrayBuffer();
+    let data4 = await resp4.arrayBuffer();
 
     var base64data1 = Buffer.from(data1).toString('base64');
     var base64data2 = Buffer.from(data2).toString('base64');
-
-    var name1 = getName();
-    var name2 = getName();
+    var base64data3 = Buffer.from(data3).toString('base64');
+    var base64data4 = Buffer.from(data4).toString('base64');
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: {
             cat1: base64data1,
             cat2: base64data2,
-            names: [name1, name2]
+            cat3: base64data3,
+            cat4: base64data4
         }
     };
-}
-
-function getName() {
-    var names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"];
-    var random_value = Math.floor(names.length * Math.random());
-    var resultName = names[random_value];
-    return resultName;
 }
