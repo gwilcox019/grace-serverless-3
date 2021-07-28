5b0793e7b972fed4d6f22a1216c7d5576540b513
danielkimtest <autumnleaf023@gmail.com>
2021-07-28 14:31:42 -0400

    arrays for distances, session IDs from MapQuest API

--- ecomaps/index.js
+++ ecomaps/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
     // parameters from user input: start and ending points, number of routes to search for
     let params = new URLSearchParams({
         "from" : req.query.start,
-        "to" : req.query.destination, //change from params to body
+        "to" : req.query.destination,
         "maxRoutes" : req.query.maxRoutes,
         "timeOverage" : 100
     })
@@ -17,10 +17,16 @@ module.exports = async function (context, req) {
     
     data = await resp.json();
     
-    // get array of distances back
-    console.log(data.route.distance);
-    console.log(data.route.alternateRoutes[0].route.distance);
-    console.log(data.route.alternateRoutes[1].route.distance);
+    // get arrays for distances, session IDs
+    const distances = [];
+    const sessionIDs = [];
+
+    distances[0] = data.route.distance;
+    sessionIDs[0] = data.route.sessionId;
+    for (i = 0; i < req.query.maxRoutes - 1; i++) {
+        distances[i + 1] = data.route.alternateRoutes[i].route.distance;
+        sessionIDs[i + 1] = data.route.alternateRoutes[i].route.sessionId;
+    }
 
     context.res = {
         body: data
