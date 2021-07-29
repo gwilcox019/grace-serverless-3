bf764262e94c9fc7d4707d28d66418d095ab755d
danielkimtest <autumnleaf023@gmail.com>
2021-07-28 22:59:29 -0400

    calls Carbon Interface API to get back emissions estimate. Still need to integrate vehicle make/model information

--- ecomaps/index.js
+++ ecomaps/index.js
@@ -28,7 +28,32 @@ module.exports = async function (context, req) {
         sessionIDs[i + 1] = data.route.alternateRoutes[i].route.sessionId;
     }
 
+    // get arrays of estimates for each route
+    let estimates = [];
+    for (i = 0; i < distances.length; i++) {
+        estimates[i] = await carbonEstimate(distances[i]);
+    }
+
     context.res = {
-        body: data
+        body: estimates
     };
+}
+
+async function carbonEstimate(distance) {
+    // need a way to get vehicle id from model?
+    let resp = await fetch("https://www.carboninterface.com/api/v1/estimates", {
+        method: 'POST',
+        headers: {
+            "Authorization": "Bearer " + process.env.CARBONINTERFACE_KEY,
+            "Content-Type": "application/json"},
+        body: JSON.stringify({
+            "type": "vehicle",
+            "distance_unit": "mi",
+            "distance_value": distance,
+            "vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"
+        })
+    })
+
+    estimateData = await resp.json();
+    return estimateData.data.attributes.carbon_lb;
 }
